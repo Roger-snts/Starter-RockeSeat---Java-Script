bb08a99c057a6a3a44ab809d9e8b60a9986f124a
Moacir Roger <100392409+Roger-snts@users.noreply.github.com>
2024-07-24 20:18:19 -0300

    Create fluxo_caixa_familiar.js

---
+++ Consolidando com Exercícios/fluxo_caixa_familiar.js
@@ -0,0 +1,18 @@
+
+/*
+    F.C.F   - Fluxo de Caixa Familiar
+
+    Crie um objeto que possuirá 2 propriedades, ambas do tipo Array:
+
+        *   receitas: []
+        *   despesas: []
+    
+    Agora, crie uma função que irá calcular o total de receitas e despesas e 
+    irá mostrar uma mensagem se a família está com o saldo positivo ou negativo, 
+    seguido do valor do saldo.
+*/
+
+
+
+
+
